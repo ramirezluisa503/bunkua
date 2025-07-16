34b2667d7e9c5823e771899431d9ea0c42fe9954
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-07-15 21:09:52 -0500

    Integración ramas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import { Services } from './Pages/Services';
-import {AboutUs} from './Pages/AboutUs';
-
+import { AboutUs } from './Pages/AboutUs';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/AboutUs" element={<AboutUs />} />
-
+      <Route
+        path="/"
+        element={<HomePage />}
+      />
+      <Route
+        path="/services"
+        element={<Services />}
+      />
+      <Route
+        path="/AboutUs"
+        element={<AboutUs />}
+      />
     </Routes>
   );
 }
